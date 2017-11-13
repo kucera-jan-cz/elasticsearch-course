a638b310745e72154caec2acc9ad51b7f7928251
Jan Kucera <kucera.jan.cz@gmail.com>
2017-11-13 21:48:09 +0100

    Added: cluster slides and exercises

--- scripts/elk_restart.sh
+++ scripts/elk_restart.sh
@@ -2,7 +2,7 @@
 IP=`hostname -I|cut -f1 -d ' '`
 
 systemctl restart elasticsearch
-curl -s http://localhost:9200/_cluster/health?wait_for_status=yellow
+curl -s -o /dev/null http://localhost:9200/_cluster/health?wait_for_status=yellow
 
 sed -i "s/^server.host:.*/server.host: \"$IP\"/g" /etc/kibana/kibana.yml
 systemctl restart kibana
